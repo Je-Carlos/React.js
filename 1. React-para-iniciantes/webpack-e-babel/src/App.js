ac5cab08f9872ccf76878113d8cccf153914c086
Je-Carlos <cpjeanberg@gmail.com>
2023-02-06 14:25:30 -0300

    webpack e babel/ configs básicas

---
+++ 1. React-para-iniciantes/webpack-e-babel/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return React.createElement(
+    "a",
+    { href: "https://www.github.com/Je-Carlos" },
+    "GitHub"
+  );
+};
+
+export default App;
